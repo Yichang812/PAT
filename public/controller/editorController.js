5c3bd5af676fa4cfabf3fc41e071c0a07b1ea52e
Li Yichang <ariellee121@gmail.com>
2015-09-08 18:41:59 +0800

    fix mode unsynchronized with content
    Now, the mode of each content can be defined individually.

--- public/controller/editorController.js
+++ public/controller/editorController.js
@@ -3,55 +3,41 @@
 patApp.controller('editorController', ['$scope',function($scope) {
     // The modes
     $scope.modes = ['Scheme', 'XML', 'Javascript'];
-    $scope.mode
-
-    $scope.cmOption  = {
-        lineNumbers: true,
-        indentWithTabs: true,
-        theme:'lesser-dark',
-        onLoad : function(_cm){
-
-            // HACK to have the codemirror instance in the scope...
-            $scope.modeChanged = function(content){
-                _cm.setOption("mode", content.mode.toLowerCase());
-            };
-        }
-    };
-
-    // Initial code content...
-    //$scope.cmModel = ';; Scheme code in here.\n' +
-    //'(define (double x)\n\t(* x x))\n\n\n' +
-    //'<!-- XML code in here. -->\n' +
-    //'<root>\n\t<foo>\n\t</foo>\n\t<bar/>\n</root>\n\n\n' +
-    //'// Javascript code in here.\n' +
-    //'function foo(msg) {\n\tvar r = Math.random();\n\treturn "" + r + " : " + msg;\n}';
+    $scope.mode = $scope.modes[0];
 
 
     //functions for tabs
 
     $scope.tabs = [{
         title: 'Model_1',
-        mode:$scope.modes[0],
         cmModel:
         ';; Scheme code in here.\n' +
         '(define (double x)\n\t(* x x))\n\n\n' +
         '<!-- XML code in here. -->\n' +
         '<root>\n\t<foo>\n\t</foo>\n\t<bar/>\n</root>\n\n\n' +
         '// Javascript code in here.\n' +
-        'function foo(msg) {\n\tvar r = Math.random();\n\treturn "" + r + " : " + msg;\n}'
+        'function foo(msg) {\n\tvar r = Math.random();\n\treturn "" + r + " : " + msg;\n}',
+        cmOption:{
+            lineNumbers: true,
+            indentWithTabs: true,
+            theme:'lesser-dark',
+            mode:$scope.mode.toLowerCase()}
     }, {
         title: 'Model_2',
-        mode:$scope.modes[0],
-        cmModel:''
+        cmModel:'',
+        cmOption:{
+            lineNumbers: true,
+            indentWithTabs: true,
+            theme:'lesser-dark',
+            mode:$scope.mode.toLowerCase()}
     }];
 
 
     $scope.tabCount = 2;
-
     $scope.currentTab = 'Model_1';
 
     $scope.isThisContent = function(content){
-        console.log($scope.currentTab);
+
         return content.title == $scope.currentTab;
     };
 
@@ -66,11 +52,15 @@ patApp.controller('editorController', ['$scope',function($scope) {
         $scope.tabCount ++;
         console.log($scope.tabCount);
         var newTitle = 'Model_' + $scope.tabCount;
-        $scope.tabs.push({title:newTitle,mode:$scope.modes[0],cmMode:''});
+        $scope.tabs.push({title:newTitle,cmMode:'',cmOption:{lineNumbers: true,indentWithTabs: true,theme:'lesser-dark',mode:$scope.mode.toLowerCase()}});
     };
     $scope.closeTab = function(tab){
         var index = $scope.tabs.indexOf(tab);
         $scope.tabs.splice(index,1);
+    };
+    $scope.changeMode = function (content,mode) {
+        content.cmOption.mode = mode.toLowerCase();
     }
+
 }]);
 
