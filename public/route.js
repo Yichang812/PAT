33180e720ce625b5cd85be727f157e94c5518ac3
Li Yichang <ariellee121@gmail.com>
2016-03-15 00:24:58 +0800

    Update route.js

--- public/route.js
+++ public/route.js
@@ -6,28 +6,25 @@ var patApp = angular.module('patApp', ['ngRoute','ui.codemirror','ngCookies']);
 patApp.config(function($routeProvider) {
     $routeProvider
 
-        // route for the home page
+        // route for the main view (editor)
         .when('/', {
             templateUrl : 'view/editor.html',
             controller  : 'editorController',
             css:'view/css/editor.css'
         })
 
-        // route for the about page
+        // route for the verification view
         .when('/verifier', {
             templateUrl : 'view/verifier.html',
             controller  : 'verifierController'
         })
 
-        // route for the contact page
+        // route for the simulation view
         .when('/simulator', {
             templateUrl : 'view/simulator.html',
             controller  : 'simulatorController'
         })
-        .when('/graph',{
-            templateUrl : 'view/graph.html',
-            controller  : 'graphController'
-        })
+        
         .otherwise({
             redirectTo:'/'
         });
